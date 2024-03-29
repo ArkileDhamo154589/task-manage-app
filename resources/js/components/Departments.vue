558a0d9c110a8d0884daf24a99d93211c6468ea2
Arqile Dhamo <arkiledamo97@gmail.com>
2024-03-29 15:26:42 +0200

    update with basic of componentθπ

---
+++ resources/js/components/Departments.vue
@@ -0,0 +1,14 @@
+<!-- we declare our template tags , we have to do this in each vue component -->
+
+<template>
+    <h1> Departments </h1>
+</template>
+
+<script>
+/*
+We have create our component but we have to import it in our application
+*/
+export default {
+
+}
+</script>
